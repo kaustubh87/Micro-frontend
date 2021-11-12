ceb792665f881b73a80fe9ba9e93b7428199d15d
kaustubh87 <kaustubh87@gmail.com>
2021-11-11 20:19:46 -0500

    Added lazy loading

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
+    <Suspense fallback={<div>Loading</div>}>
+      <Header />
+    </Suspense>
     <div className="my-10">PDP Page Content</div>
     <Footer />
   </div>
